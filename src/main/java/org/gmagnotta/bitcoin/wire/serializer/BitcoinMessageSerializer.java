98ef28b7c25fdb8420e3ed4f25e2470e93f5fd39
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2018-01-05 17:33:27 +0100

    add javadoc

--- src/main/java/org/gmagnotta/bitcoin/wire/serializer/BitcoinMessageSerializer.java
+++ src/main/java/org/gmagnotta/bitcoin/wire/serializer/BitcoinMessageSerializer.java
@@ -4,8 +4,22 @@ import org.gmagnotta.bitcoin.message.BitcoinMessage;
 
 public interface BitcoinMessageSerializer {
 	
-	BitcoinMessage deserialize(byte[] payload) throws BitcoinMessageSerializerException;
+	/**
+	 * Deserialize an array of byte to a BitcoinMessage
+	 * 
+	 * @param payload
+	 * @return
+	 * @throws BitcoinMessageSerializerException
+	 */
+	public BitcoinMessage deserialize(byte[] payload) throws BitcoinMessageSerializerException;
 	
-	byte[] serialize(BitcoinMessage messageToSerialize) throws BitcoinMessageSerializerException;
+	/**
+	 * Serialize a BitcoinMessage to a byte array
+	 * 
+	 * @param messageToSerialize
+	 * @return
+	 * @throws BitcoinMessageSerializerException
+	 */
+	public byte[] serialize(BitcoinMessage messageToSerialize) throws BitcoinMessageSerializerException;
 
 }
